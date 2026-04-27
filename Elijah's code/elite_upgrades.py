a3a5eadcb696728980854d83dc198ab3e1be9a9f
Bruhster <elijah.hawkins@ucas-edu.net>
2026-04-27 16:50:35 +0000

    bruh2

--- Elijah's code/elite_upgrades.py
+++ Elijah's code/elite_upgrades.py
@@ -1,13 +1,19 @@
 #Upgrades for the Miner Clicker game
 import math
 
+def on_click(level):
+    click = False
+
+    if click == True:
+        return Upgrade("Click Upgrade", 10, 1.5, 0.5)
+
 class Upgrade:
     def __init__(self, name, base_cost, cost_multiplier, effect_multiplier):
         self.name = name
         self.base_cost = base_cost
         self.cost_multiplier = cost_multiplier
         self.effect_multiplier = effect_multiplier
-        self.upgrades = 0
+        self.level = 0
 
     def get_cost(self):
         return math.ceil(self.base_cost * (self.cost_multiplier ** self.level))
@@ -16,9 +22,8 @@ class Upgrade:
         return 1 + (self.effect_multiplier * self.level)
 
     def purchase(self):
-        self.upgrades += 1
+        self.level += 1
 
 class ClickUpgrade(Upgrade):
     def __init__(self, name, base_cost, cost_multiplier, effect_multiplier):
         super().__init__(name, base_cost, cost_multiplier, effect_multiplier)
-        
--- Elijah's code/upgrade_logic.py
+++ Elijah's code/upgrade_logic.py
@@ -1,6 +1,14 @@
 
+import math
+
+from elite_upgrades import *
+
 def math_for_click_upgrade(level):
     return 1 + (0.5 * level)
 
 def math_for_exponential_cost_increase(base_cost, cost_multiplier, level):
-    return base_cost * (cost_multiplier ** level)
+    return base_cost * (cost_multiplier ** level) 
+
+def math_for_effect_multiplier(base_effect, effect_multiplier, level):
+    return base_effect * (1 + (effect_multiplier * level))
+
