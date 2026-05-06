780f3eed1ac8d3d415ef9650031595a170815091
Bruhster <elijah.hawkins@ucas-edu.net>
2026-05-06 16:50:37 +0000

    bruh

--- Elijah's code/elite_upgrades.py
+++ Elijah's code/elite_upgrades.py
@@ -4,12 +4,13 @@ import math
 from buildings import *
 
 class Upgrade:
-    def __init__(self, name, base_cost, cost_multiplier, effect_multiplier):
+    def __init__(self, name, base_cost, cost_multiplier, effect_multiplier, description="", image):
         self.name = name
         self.base_cost = base_cost
         self.cost_multiplier = cost_multiplier
         self.effect_multiplier = effect_multiplier
         self.level = 0
+        self.description = description
 
     def get_cost(self):
         return math.ceil(self.base_cost * (self.cost_multiplier ** self.level))
@@ -20,57 +21,107 @@ class Upgrade:
     def purchase(self):
         self.level += 1
 
+click_description = "Increases your mining power! Increases the number of cookies you get per click by 0.5 for each level of this upgrade. The cost of this upgrade increases by 50% with each purchase."
+
+pickaxe_description = "Increases the speed of your Pickaxes! Increases the multiplier for the Pickaxe building by 0.5 for each level of this upgrade. The cost of this upgrade doubles with each purchase."
+
+alien_description = "Increases the stamina of your Aliens! Increases the multiplier for the Alien building by 1 for each level of this upgrade. The cost of this upgrade increases by 150% with each purchase."
+
+grinder_description = "Make your Grinders sharper! Increases the multiplier for the Grinder building by 2 for each level of this upgrade. The cost of this upgrade triples with each purchase."
+
+quarry_description = "Vehicles in your Quarry will now go faster and have more carrying capacity! Increases the multiplier for the Quarry building by 5 for each level of this upgrade. The cost of this upgrade increases by 250% with each purchase."
+
+rock_factory_description = "Your rock factories will now produce more rocks! Increases the multiplier for the Rock Factory building by 10 for each level of this upgrade. The cost of this upgrade quadruples with each purchase."
+
+asteroid_portal_description = "Travel faster through the Asteroid Portal! Increases the multiplier for the Asteroid Portal building by 20 for each level of this upgrade. The cost of this upgrade increases by 350% with each purchase."
+
+time_machine_description = "Your Time Machine will now mine more rocks from the past! Increases the multiplier for the Time Machine building by 50 for each level of this upgrade. The cost of this upgrade quintuples with each purchase."
+
+quantum_drill_description = "Your Quantum Drill will now mine more rocks from the quantum realm! Increases the multiplier for the Quantum Drill building by 100 for each level of this upgrade. The cost of this upgrade increases by 450% with each purchase."
+
+alien_planet_description = "Your Alien Planet will now produce more exotic rocks! Increases the multiplier for the Alien Planet building by 200 for each level of this upgrade. The cost of this upgrade sextuples with each purchase."
+
+dyson_sphere_description = "Your Dyson Sphere will now harness more energy from the stars! Increases the multiplier for the Dyson Sphere building by 500 for each level of this upgrade. The cost of this upgrade increases by 550% with each purchase."
+
+galaxy_cluster_description = "Your Galaxy Cluster will now produce more cosmic dust! Increases the multiplier for the Galaxy Cluster building by 1000 for each level of this upgrade. The cost of this upgrade septuples with each purchase."
+
+multiverse_description = "Your Multiverse will now produce more multiversal energy! Increases the multiplier for the Multiverse building by 2000 for each level of this upgrade. The cost of this upgrade increases by 650% with each purchase."
+
+all_building_description = "All of your buildings will now produce more rocks in total! Increases the multiplier for all buildings by 5000 for each level of this upgrade. The cost of this upgrade octuples with each purchase."
+
+acid_description = "Your acid will now dissolve rocks faster! Increases the multiplier for all buildings by 1.5 for each level of this upgrade. The cost of this upgrade doubles with each purchase."
+
+pickaxe_efficiency_description = "Your pickaxes will now be more efficient! Increases the multiplier for all buildings by 1.5 for each level of this upgrade. The cost of this upgrade increases by 150% with each purchase."
+
+pure_luck_description = "Your miners will now have better luck finding rocks! Increases the multiplier for all buildings by 1 for each level of this upgrade. The cost of this upgrade triples with each purchase."
+
+miners_intuition_description = "Your miners will now have better intuition about where to find rocks! Increases the multiplier for all buildings by 2 for each level of this upgrade. The cost of this upgrade increases by 250% with each purchase."
+
+get_a_foundry_description = "You will now have a foundry to process your rocks! Increases the multiplier for all buildings by 5 for each level of this upgrade. The cost of this upgrade increases by 350% with each purchase."
+
+better_tools_description = "Your tools will now be better quality! Increases the multiplier for all buildings by 10 for each level of this upgrade. The cost of this upgrade increases by 450% with each purchase."
+
+mining_bullets_description = "Your mining bullets will now be more effective! Increases the multiplier for all buildings by 20 for each level of this upgrade. The cost of this upgrade increases by 550% with each purchase."
+
+asteroid_mining_description = "You will now be able to mine asteroids! Increases the multiplier for all buildings by 50 for each level of this upgrade. The cost of this upgrade increases by 650% with each purchase."
+
+diamond_pickaxe_description = "Your pickaxe will now be made of diamond! Increases the multiplier for all buildings by 100 for each level of this upgrade. The cost of this upgrade increases by 750% with each purchase."
+
+alien_technology_description = "You will now have access to alien technology! Increases the multiplier for all buildings by 200 for each level of this upgrade. The cost of this upgrade increases by 850% with each purchase."
+
+improved_solar_panels_description = "Your solar panels will now be more efficient and bigger! Increases the multiplier for all buildings by 500 for each level of this upgrade. The cost of this upgrade increases by 950% with each purchase."
+
+insane_aliens_description = "Your aliens will now be more insane and productive! Increases the multiplier for all buildings by 1000 for each level of this upgrade. The cost of this upgrade increases by 1050% with each purchase."
+
 upgrade_stats = {
-    "click": ("Click Upgrade", 10, 1.5, 0.5),
-    "pickaxe": ("Pickaxe Upgrade", 100, 2, 0.5),
-    "alien": ("Alien Upgrade", 1000, 2.5, 1),
-    "small_mine": ("Grinder Upgrade", 11000, 3, 2),
-    "massive_mine": ("Massive Mine Upgrade", 120000, 3.5, 5),
-    "rock_factory": ("Rock Factory Upgrade", 1300000, 4, 10),
-    "asteroid_portal": ("Portal to Asteroid Upgrade", 10**6, 4.5, 20),
-    "time_machine": ("Time Machine Upgrade", 14 * 10**12, 5, 50),
-    "quantum_drill": ("Quantum Drill Upgrade", 1.4 * 10**15, 5.5, 100),
-    "alien_planet": ("Alien Planet Upgrade", 1.4 * 10**18, 6, 200),
-    "dyson_sphere": ("Dyson Sphere Upgrade", 1.4 * 10**21, 6.5, 500),
-    "galaxy_cluster": ("Galaxy Cluster Upgrade", 1.4 * 10**24, 7, 1000),
-    "multiverse": ("Multiverse Upgrade", 1.4 * 10**27, 7.5, 2000),
-    "all_building": ("All Building Upgrade", 1.4 * 10**30, 8, 5000)
+    "click": ("Click Upgrade", 10, 1.5, 0.5, click_description),
+    "pickaxe": ("Pickaxe Upgrade", 100, 2, 0.5, pickaxe_description),
+    "Alien": ("Alien Upgrade", 1000, 2.5, 1, alien_description),
+    "Grinder": ("Grinder Upgrade", 11000, 3, 2, grinder_description),
+    "Quarry": ("Massive Mine Upgrade", 120000, 3.5, 5, quarry_description),
+    "rock_factory": ("Rock Factory Upgrade", 1300000, 4, 10, rock_factory_description),
+    "asteroid_portal": ("Portal to Asteroid Upgrade", 10**6, 4.5, 20, asteroid_portal_description),
+    "time_machine": ("Time Machine Upgrade", 14 * 10**12, 5, 50, time_machine_description),
+    "quantum_drill": ("Quantum Drill Upgrade", 1.4 * 10**15, 5.5, 100, quantum_drill_description),
+    "alien_planet": ("Alien Planet Upgrade", 1.4 * 10**18, 6, 200, alien_planet_description),
+    "dyson_sphere": ("Dyson Sphere Upgrade", 1.4 * 10**21, 6.5, 500, dyson_sphere_description),
+    "galaxy_cluster": ("Galaxy Cluster Upgrade", 1.4 * 10**24, 7, 1000, galaxy_cluster_description),
+    "multiverse": ("Multiverse Upgrade", 1.4 * 10**27, 7.5, 2000, multiverse_description),
+    "all_building": ("All Building Upgrade", 1.4 * 10**30, 8, 5000, all_building_description)
 }
 
-tier_2_upgrades = {
-    "click": ("Click Upgrade Tier 2", 10**6, 2, 1),
-    "pickaxe": ("Pickaxe Upgrade Tier 2", 10**7, 2.5, 1),
-    "alien": ("Alien Upgrade Tier 2", 10**8, 3, 2),
-    "small_mine": ("Grinder Upgrade Tier 2", 10**9, 3.5, 5),
-    "massive_mine": ("Massive Mine Upgrade Tier 2", 10**10, 4, 10),
-    "rock_factory": ("Rock Factory Upgrade Tier 2", 10**11, 4.5, 20),
-    "asteroid_portal": ("Portal to Asteroid Upgrade Tier 2", 10**12, 5, 50),
-    "time_machine": ("Time Machine Upgrade Tier 2", 10**13, 5.5, 100),
-    "quantum_drill": ("Quantum Drill Upgrade Tier 2", 10**14, 6, 200),
-    "alien_planet": ("Alien Planet Upgrade Tier 2", 10**15, 6.5, 500),
-    "dyson_sphere": ("Dyson Sphere Upgrade Tier 2", 10**16, 7, 1000),
-    "galaxy_cluster": ("Galaxy Cluster Upgrade Tier 2", 10**17, 7.5, 2000),
-    "multiverse": ("Multiverse Upgrade Tier 2", 10**18, 8, 5000),
-    "all_building": ("All Building Upgrade Tier 2", 10**19, 8.5, 10000)
+regular_upgrades = {
+    "Acid": ("Acid Upgrade", 10**5, 2, 0.5, acid_description),
+    "Pickaxe Efficiency": ("Pickaxe Efficiency Upgrade", 10**6, 2.5, 0.5, pickaxe_efficiency_description),
+    "Pure Luck": ("Pure Luck Upgrade", 10**7, 3, 1, pure_luck_description),
+    "Miner's Intuition": ("Miner's Intuition Upgrade", 10**8, 3.5, 2, miners_intuition_description),
+    "Get a Foundry": ("Get a Foundry Upgrade", 10**9, 4, 5, get_a_foundry_description),
+    "Better Tools": ("Better Tools Upgrade", 10**10, 4.5, 10, better_tools_description),
+    "Mining Bullets": ("Mining Bullets Upgrade", 10**11, 5, 20, mining_bullets_description),
+    "Asteroid Mining": ("Asteroid Mining Upgrade", 10**12, 5, 50, asteroid_mining_description),
+    "Diamond Pickaxe": ("Diamond Pickaxe Upgrade", 10**13, 5.5, 100, diamond_pickaxe_description),
+    "Alien Technology": ("Alien Technology Upgrade", 10**14, 6, 200, alien_technology_description),
+    "Improved Solar Panels": ("Improved Solar Panels Upgrade", 10**15, 6.5, 500, improved_solar_panels_description),
+    "Insane Aliens": ("Insane Aliens Upgrade", 10**16, 7, 1000, insane_aliens_description)
 }
 
 def get_upgrade(key):
     if key in upgrade_stats:
-        name, base_cost, cost_multiplier, effect_multiplier = upgrade_stats[key]
-        return [Upgrade(name, base_cost, cost_multiplier, effect_multiplier)]
+        name, base_cost, cost_multiplier, effect_multiplier, description = upgrade_stats[key]
+        return [Upgrade(name, base_cost, cost_multiplier, effect_multiplier, description)]
         
-    elif key in tier_2_upgrades:
-        name, base_cost, cost_multiplier, effect_multiplier = tier_2_upgrades[key]
-        return [Upgrade(name, base_cost, cost_multiplier, effect_multiplier)]
+    elif key in regular_upgrades:
+        name, base_cost, cost_multiplier, effect_multiplier, description = regular_upgrades[key]
+        return [Upgrade(name, base_cost, cost_multiplier, effect_multiplier, description)]
         
     else:
         raise ValueError("Invalid upgrade key")
-    
+
 def get_all_upgrades():
     all_upgrades = []
     for key in upgrade_stats:
         all_upgrades.append(get_upgrade(key)[0])
-    for key in tier_2_upgrades:
+    for key in regular_upgrades:
         all_upgrades.append(get_upgrade(key)[0])
     return all_upgrades
 
@@ -78,7 +129,7 @@ def get_cost(upgrade_key):
     if upgrade_key in upgrade_stats:
         upgrade = get_upgrade(upgrade_key)[0]
         return upgrade.get_cost()
-    elif upgrade_key in tier_2_upgrades:
+    elif upgrade_key in regular_upgrades:
         upgrade = get_upgrade(upgrade_key)[0]
         return upgrade.get_cost()
     else:
@@ -110,31 +161,3 @@ def upgrade_building(building_index, upgrade_key):
         building = buildings[building_index]
         building["mult"] += upgrade.get_effect()
         upgrade.purchase_upgrade()
-
-click_description = "Increases your mining power! Increases the number of cookies you get per click by 0.5 for each level of this upgrade. The cost of this upgrade increases by 50% with each purchase."
-
-pickaxe_description = "Increases the speed of your Pickaxes! Increases the multiplier for the Pickaxe building by 0.5 for each level of this upgrade. The cost of this upgrade doubles with each purchase."
-
-alien_description = "Increases the stamina of your Aliens! Increases the multiplier for the Alien building by 1 for each level of this upgrade. The cost of this upgrade increases by 150% with each purchase."
-
-grinder_description = "Make your Grinders sharper! Increases the multiplier for the Grinder building by 2 for each level of this upgrade. The cost of this upgrade triples with each purchase."
-
-quarry_description = "Vehicles in your Quarry will now go faster and have more carrying capacity! Increases the multiplier for the Quarry building by 5 for each level of this upgrade. The cost of this upgrade increases by 250% with each purchase."
-
-rock_factory_description = "Your rock factories will now produce more rocks! Increases the multiplier for the Rock Factory building by 10 for each level of this upgrade. The cost of this upgrade quadruples with each purchase."
-
-asteroid_portal_description = "Travel faster through the Asteroid Portal! Increases the multiplier for the Asteroid Portal building by 20 for each level of this upgrade. The cost of this upgrade increases by 350% with each purchase."
-
-time_machine_description = "Your Time Machine will now mine more rocks from the past! Increases the multiplier for the Time Machine building by 50 for each level of this upgrade. The cost of this upgrade quintuples with each purchase."
-
-quantum_drill_description = "Your Quantum Drill will now mine more rocks from the quantum realm! Increases the multiplier for the Quantum Drill building by 100 for each level of this upgrade. The cost of this upgrade increases by 450% with each purchase."
-
-alien_planet_description = "Your Alien Planet will now produce more exotic rocks! Increases the multiplier for the Alien Planet building by 200 for each level of this upgrade. The cost of this upgrade sextuples with each purchase."
-
-dyson_sphere_description = "Your Dyson Sphere will now harness more energy from the stars! Increases the multiplier for the Dyson Sphere building by 500 for each level of this upgrade. The cost of this upgrade increases by 550% with each purchase."
-
-galaxy_cluster_description = "Your Galaxy Cluster will now produce more cosmic dust! Increases the multiplier for the Galaxy Cluster building by 1000 for each level of this upgrade. The cost of this upgrade septuples with each purchase."
-
-multiverse_description = "Your Multiverse will now produce more multiversal energy! Increases the multiplier for the Multiverse building by 2000 for each level of this upgrade. The cost of this upgrade increases by 650% with each purchase."
-
-all_building_description = "All of your buildings will now produce more rocks in total! Increases the multiplier for all buildings by 5000 for each level of this upgrade. The cost of this upgrade octuples with each purchase."
