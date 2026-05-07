dd435ed482b1ebb88147d699d3840ce658f48e1d
CheesyDevil <seth.white@ucas-edu.net>
2026-05-07 10:34:13 -0600

    started main file

--- UI/seth_click.py
+++ UI/seth_click.py
@@ -3,8 +3,7 @@ import sys
 
 p.init()
 
-res=(720,720)
-screen = p.display.set_mode(res,p.RESIZABLE)
+screen = p.display.set_mode((720,720),p.RESIZABLE)
 
 
 
@@ -25,6 +24,9 @@ button_mult=1
 
 
 def click(mouse,upgrades,counter, buildings):
+    def button_click(button_flat,button_mult,counter):
+        counter+=button_flat*button_mult
+        return counter
     #Buildings
     if width/12*7<=mouse[0]<=width/12*11 and height/12*11<=mouse[1]<=height:
         if buildings[0].get_cost()<=counter:
--- UI/seth_hover.py
+++ UI/seth_hover.py
@@ -17,6 +17,7 @@ color4=(255,255,255)
 
 
 desc_font=p.font.SysFont('Corbeal',35)
+coun_font=p.font.SysFont('Corbeal',40)
 
 def load_image(image,coords,scale):
     oimage=p.image.load(image)
@@ -30,219 +31,219 @@ def hover(mouse,counter,buildings,upgrades):
     if width/12*7<=mouse[0]<=width/12*11 and height/12*11<=mouse[1]<=height:
         p.draw.rect(screen,color2,[width/12*7,0,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[0].description()
+        text=buildings[0].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*7<=mouse[0]<=width/12*11 and height/12*10<=mouse[1]<=height/12*11:
         p.draw.rect(screen,color2,[width/12*7,height/12,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[1].description()
+        text=buildings[1].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1])) 
     if width/12*7<=mouse[0]<=width/12*11 and height/12*9<=mouse[1]<=height/12*10:
         p.draw.rect(screen,color2,[width/12*7,height/12*2,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[2].description()
+        text=buildings[2].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*7<=mouse[0]<=width/12*11 and height/12*8<=mouse[1]<=height/12*9:
         p.draw.rect(screen,color2,[width/12*7,height/12*3,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[3].description()
+        text=buildings[3].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*7<=mouse[0]<=width/12*11 and height/12*7<=mouse[1]<=height/12*8:
         p.draw.rect(screen,color2,[width/12*7,height/12*4,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[4].description()
+        text=buildings[4].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*7<=mouse[0]<=width/12*11 and height/12*6<=mouse[1]<=height/12*7:
         p.draw.rect(screen,color2,[width/12*7,height/12*5,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[5].description()
+        text=buildings[5].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*7<=mouse[0]<=width/12*11 and height/12*5<=mouse[1]<=height/12*6:
         p.draw.rect(screen,color2,[width/12*7,height/12*6,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[6].description()
+        text=buildings[6].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*7<=mouse[0]<=width/12*11 and height/12*4<=mouse[1]<=height/12*5:
         p.draw.rect(screen,color2,[width/12*7,height/12*7,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[7].description()
+        text=buildings[7].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*7<=mouse[0]<=width/12*11 and height/12*3<=mouse[1]<=height/12*4:
         p.draw.rect(screen,color2,[width/12*7,height/12*8,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[8].description()
+        text=buildings[8].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*7<=mouse[0]<=width/12*11 and height/12*2<=mouse[1]<=height/12*3:
         p.draw.rect(screen,color2,[width/12*7,height/12*9,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[9].description()
+        text=buildings[9].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*7<=mouse[0]<=width/12*11 and height/12<=mouse[1]<=height/12*2:
         p.draw.rect(screen,color2,[width/12*7,height/12*10,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[10].description()
+        text=buildings[10].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*7<=mouse[0]<=width/12*11 and 0<=mouse[1]<=height/12:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=buildings[11].description()
+        text=buildings[11].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     #building upgrades
     if width/12*11<=mouse[0]<=width and height/12*11<=mouse[1]<=height:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[0].description()
+        text=upgrades[0].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and height/12*10<=mouse[1]<=height/12*11:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[1].description()
+        text=upgrades[1].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and height/12*9<=mouse[1]<=height/12*10:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[2].description()
+        text=upgrades[2].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and height/12*8<=mouse[1]<=height/12*9:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[3].description()
+        text=upgrades[3].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and height/12*7<=mouse[1]<=height/12*8:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[4].description()
+        text=upgrades[4].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and height/12*6<=mouse[1]<=height/12*7:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[5].description()
+        text=upgrades[5].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and height/12*5<=mouse[1]<=height/12*6:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[6].description()
+        text=upgrades[6].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and height/12*4<=mouse[1]<=height/12*5:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[7].description()
+        text=upgrades[7].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and height/12*3<=mouse[1]<=height/12*4:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[8].description()
+        text=upgrades[8].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and height/12*2<=mouse[1]<=height/12*3:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[9].description()
+        text=upgrades[9].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and height/12<=mouse[1]<=height/12*2:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[10].description()
+        text=upgrades[10].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if width/12*11<=mouse[0]<=width and 0<=mouse[1]<=height/12:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[11].description()
+        text=upgrades[11].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     #other upgrades
     if 0<=mouse[0]<=width/6 and height/12*11<=mouse[1]<=height:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[12].description()
+        text=upgrades[12].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and height/12*10<=mouse[1]<=height/12*11:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[13].description()
+        text=upgrades[13].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and height/12*9<=mouse[1]<=height/12*10:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[14].description()
+        text=upgrades[14].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and height/12*8<=mouse[1]<=height/12*9:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[15].description()
+        text=upgrades[15].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and height/12*7<=mouse[1]<=height/12*8:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[16].description()
+        text=upgrades[16].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and height/12*6<=mouse[1]<=height/12*7:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[17].description()
+        text=upgrades[17].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and height/12*5<=mouse[1]<=height/12*6:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[18].description()
+        text=upgrades[18].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and height/12*4<=mouse[1]<=height/12*5:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[19].description()
+        text=upgrades[19].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and height/12*3<=mouse[1]<=height/12*4:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[20].description()
+        text=upgrades[20].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and height/12*2<=mouse[1]<=height/12*3:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[21].description()
+        text=upgrades[21].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and height/12<=mouse[1]<=height/12*2:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[22].description()
+        text=upgrades[22].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     if 0<=mouse[0]<=width/6 and 0<=mouse[1]<=height/12:
         p.draw.rect(screen,color2,[width/12*7,height/12*11,width/3,height/12])
         p.draw.rect(screen,color3,[mouse[0]-width/4,mouse[1],width/4,height/6])
-        text=upgrades[23].description()
+        text=upgrades[23].description
         textbox=desc_font.render(text,True,color4)
         screen.blit(textbox,(mouse[0]-width/4,mouse[1]))
     #prestige
@@ -295,6 +296,10 @@ def hover(mouse,counter,buildings,upgrades):
         load_image("asteroid.png",(width/4,height/12*7),(width/4,height/4))
         p.draw.rect(screen,color1,[width/4,height/12*7,width/4,height/4])
         pass #button_click
+    #Counter
+    c_text=counter
+    c_box=coun_font.render(c_text,True,color4)
+    screen.blit(c_box,(width/6,height/12*11))
 while True:
     p.draw.circle(screen,color1,[width/8*3,height/12*5],width/8)
     p.draw.rect(screen,color1,[width/12*7,0,width/3,height/12])
